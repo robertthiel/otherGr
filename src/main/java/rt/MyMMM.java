18ed138257d4da63e5b1dc70a141fa0b44002d95
Robert Thiel <robertthiel@yahoo.com>
2023-01-23 16:29:38 -0800

    okay

---
+++ src/main/java/rt/MyMMM.java
@@ -0,0 +1,13 @@
+package rt;
+
+public class MyMMM {
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 1000; i++ ) {
+            System.out.println("super senf");
+        }
+
+    }
+}
--- src/main/java/rt/MyMain.java
+++ src/main/java/rt/MyMain.java
@@ -1,5 +1,8 @@
 package rt;
 
+
+
+
 public class MyMain {
 
 
