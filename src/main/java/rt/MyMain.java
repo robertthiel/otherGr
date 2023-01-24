08a55f8c820780ae72ee56211f79293c1101ef82
Robert Thiel <robertthiel@yahoo.com>
2023-01-23 16:03:32 -0800

    third

--- src/main/java/rt/MyMain.java
+++ src/main/java/rt/MyMain.java
@@ -1,2 +1,17 @@
-package rt;public class MyMain {
+package rt;
+
+public class MyMain {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("hello, rob");
+
+
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+        }
+    }
+
+
 }
